a0abf8961646802af4db1d7ee3eba52f2f3049dc
ibadsaleem <ibadsaleem@hotmail.com>
2022-10-28 17:26:00 +0500

    'Updated'

--- Screens/Announcement.js
+++ Screens/Announcement.js
@@ -23,12 +23,13 @@ const Announcements = props => {
   const teacherName = route.params['teacherName'];
   const id = route.params['id'];
   const [annoucements, setannoucements] = useState([]);
+  const [annoucementtext, setannoucementtext] = useState('');
   const [isTeacher, setIsTeacher] = useState(true);
   const [loading, setloading] = useState(true);
-  let loginMember='';
+  let loginMember = '';
   useEffect(() => {
     func();
-  });
+  }, []);
 
   const func = async () => {
     let jsonValue = await AsyncStorage.getItem('userinfo');
@@ -47,6 +48,31 @@ const Announcements = props => {
       .then(async json => {
         setannoucements(json);
         setloading(false);
+      });
+  };
+  const func_postannouncement = async (a) => {
+    let jsonValue = await AsyncStorage.getItem('userinfo');
+    // loginMember = await AsyncStorage.getItem('loginMember');
+    fetch(
+      `https://ipt-lms-1.herokuapp.com/api/teacher/Teacher/announcement/add/${id}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + JSON.parse(jsonValue).token,
+        },
+        body: JSON.stringify({
+          description: annoucementtext,
+          title: a,
+          announcementType: a,
+          dueDate: '2022-10-28T11:41:52.451Z',
+        }),
+      },
+    )
+      .then(response => response.json())
+      .then(json => {
+        setannoucementtext('');
+        // console.log(json.message);
       });
   };
 
@@ -88,57 +114,70 @@ const Announcements = props => {
             {titleCase(teacherName)}
           </Text>
         </View>
-        {loginMember=='student'? null:<View
-          style={{
-            width: '95%',
-            alignSelf: 'center',
-            borderWidth: 0.8,
-            borderRadius: 10,
-            padding: 10,
-            marginTop: 10,
-            borderColor: 'lightgrey',
-            flexDirection: 'row',
-          }}>
-          <View style={{width: '10%', marginRight: 20}}>
-            <Image
-              source={require('../media/FAST.png')}
-              style={{width: 45, height: 45, borderRadius: 55}}
-            />
-          </View>
-          <View style={{width: '75%'}}>
-            <TextInput
-              placeholder="Share with your class"
-              multiline={true}></TextInput>
-          </View>
-          <View style={{justifyContent: 'center', alignContent: 'flex-end'}}>
-            <TouchableOpacity>
-              <MaterialCommunityIcons
-                name="send-circle"
-                size={30}
-                color="blue"
+        {loginMember == 'student' ? null : (
+          <View
+            style={{
+              width: '95%',
+              alignSelf: 'center',
+              borderWidth: 0.8,
+              borderRadius: 10,
+              padding: 10,
+              marginTop: 10,
+              borderColor: 'lightgrey',
+              flexDirection: 'row',
+            }}>
+            <View style={{width: '10%', marginRight: 20}}>
+              <Image
+                source={require('../media/FAST.png')}
+                style={{width: 45, height: 45, borderRadius: 55}}
               />
+            </View>
+            <View style={{width: '75%'}}>
+              <TextInput
+                onChangeText={text => setannoucementtext(text)}
+                placeholder="Share with your class"
+                multiline={true}
+                  value={annoucementtext}
+                >
+                </TextInput>
+            </View>
+            <TouchableOpacity style={{justifyContent: 'center', alignContent: 'flex-end'}}>
+              <TouchableOpacity onPress={()=>func_postannouncement('ANNOUNCEMENT')}>
+                <MaterialCommunityIcons
+                  name="send-circle"
+                  size={30}
+                  color="blue"
+                />
+              </TouchableOpacity>
             </TouchableOpacity>
           </View>
-        </View>}
+        )}
         {loading ? (
           <View>
-          <ActivityIndicator size="large" color="black" style={{marginTop:200}} />
-          <Text style={{textAlign:'center',color:'black'}}>Loading...</Text>
+            <ActivityIndicator
+              size="large"
+              color="black"
+              style={{marginTop: 200}}
+            />
+            <Text style={{textAlign: 'center', color: 'black'}}>
+              Loading...
+            </Text>
           </View>
         ) : (
-          
-            annoucements.map((item, index) => {
+          annoucements.map((item, index) => {
             return (
               <AnnouncementCard
+                key={index}
                 teacher={titleCase(teacherName)}
                 description={titleCase(item.description)}
               />
             );
           })
         )}
-
       </ScrollView>
-      {loginMember=='student' ? <BottomTab/>: (
+      {loginMember == 'student' ? (
+        <BottomTab />
+      ) : (
         <FloatingAction
           actions={actions}
           onPressItem={name => {
