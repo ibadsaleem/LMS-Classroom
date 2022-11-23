86642ce069420cc1effae802f9a68983b9e69c21
ibadsaleem <ibadsaleem@hotmail.com>
2022-11-23 20:43:37 +0500

    'Updated'

--- Screens/AnnouncementView.js
+++ Screens/AnnouncementView.js
@@ -25,19 +25,22 @@ import moment from 'moment/moment';
 import RNFetchBlob from 'rn-fetch-blob';
 
 const AnnouncementView = props => {
+  const [userAssignment, setuserAssignment] = useState([]);
+  const [fileSubmitted, setFileSubmitted] = useState([]);
+  const [isFileSubmitted, setisFileSubmitted] = useState(true);
   const navigation = useNavigation();
   const route = useRoute();
   const [attachmentCount, setattachmentCount] = useState(0);
   const [loading, setLoading] = useState(false);
   const content = route.params['obj'];
   const date = route.params['date'];
-  const [loginMember,setLoginMember]= useState('');
+  const [loginMember, setLoginMember] = useState('');
   const [media, setMedia] = useState([]);
-  const [type,setType]=useState('');
+  const [type, setType] = useState('');
   useEffect(() => {
+    // checkFileSubmitted();
 
-    
-    func()  
+    func();
     const backAction = () => {
       navigation.goBack();
       return true;
@@ -48,8 +51,7 @@ const AnnouncementView = props => {
       backAction,
     );
     return () => backHandler.remove();
-    
-  },[])
+  }, []);
 
   const docPicker = async () => {
     setattachmentCount(0);
@@ -61,17 +63,16 @@ const AnnouncementView = props => {
   };
 
   const documentUpload = async () => {
-    if (attachmentCount==0){
-      alert("Please attach file")
-    }else{
-
+    if (attachmentCount == 0) {
+      alert('Please attach file');
+    } else {
       setLoading(true);
       const doc = new FormData();
       media.forEach(element => {
         doc.append('fileToUpload', element);
       });
       let jsonValue = await AsyncStorage.getItem('userinfo');
-      let id = content.obj[0].announcementId      
+      let id = content.obj[0].announcementId;
       fetch(
         `https://ipt-lms-1.herokuapp.com/api/user/Users/upload/mobile/assignment/${id}`,
         {
@@ -82,18 +83,16 @@ const AnnouncementView = props => {
           },
           body: doc,
         },
-        )
+      )
         .then(response => response.json())
         .then(data => {
-          console.log(data)
-          if (data.success==1)
-          {
+          console.log(data);
+          if (data.success == 1) {
             setMedia([]);
             setattachmentCount(0);
             setLoading(false);
             alert('Assignment Submitted Successfully');
-          }
-          else{
+          } else {
             setMedia([]);
             setattachmentCount(0);
             alert('Something went wrong');
@@ -101,43 +100,70 @@ const AnnouncementView = props => {
           }
         })
         .catch(error => {
-          setLoading(false)
-          
+          setLoading(false);
+
           console.error(error);
         });
-      }
-    
+    }
   };
 
+  const checkFileSubmitted = async () => {
+    let jsonValue = await AsyncStorage.getItem('userinfo');
 
-
-  const func=async()=>{
-    let _loginMember = await AsyncStorage.getItem('loginMember');
-    setLoginMember(_loginMember);
-    setType(content.type)
-  }
-  
-  const downloadFile=async(link,fileid)=>{
-    let jsonValue=await AsyncStorage.getItem('userinfo');
-    console.log(fileid)
     fetch(
-    `https://ipt-lms-1.herokuapp.com/submissions/Files/${fileid}`,
-    {
+      `https://ipt-lms-1.herokuapp.com/api/user/Users/submissions/Files/${content.obj[0].announcementId}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + JSON.parse(jsonValue).token,
+        },
+      },
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      });
+  };
+  const func = async () => {
+    let _loginMember = await AsyncStorage.getItem('loginMember');
+    let jsonValue = await AsyncStorage.getItem('userinfo');
+    setLoginMember(_loginMember);
+    setType(content.type);
+    fetch(
+      `https://ipt-lms-1.herokuapp.com/api/teacher/Teacher/assignments/${content.obj[0].announcementId}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + JSON.parse(jsonValue).token,
+        },
+      },
+    )
+      .then(response => response.json())
+      .then(json => {
+        setuserAssignment(json);
+      });
+  };
+
+  const downloadFile = async (link, fileid) => {
+    let jsonValue = await AsyncStorage.getItem('userinfo');
+    console.log(fileid);
+    fetch(`https://ipt-lms-1.herokuapp.com/submissions/Files/${fileid}`, {
       method: 'GET',
       headers: {
         // 'Content-Type': 'application/json',
         Authorization: 'Bearer ' + JSON.parse(jsonValue).token,
-      }
+      },
     })
-    .then(response => response.json())
-    .then(json => {
+      .then(response => response.json())
+      .then(json => {
         console.log(json);
-      
-    });
-}
+      });
+  };
   return (
     <View style={{width: '100%', height: '100%', backgroundColor: '#ffffff'}}>
-      <Header title={'Title: '+content.title} />
+      <Header title={'Title: ' + content.title} />
 
       <View>
         <View
@@ -194,37 +220,142 @@ const AnnouncementView = props => {
             borderColor: 'lightgrey',
             justifyContent: 'center',
           }}>
-          {content.obj.length==0?<Text style={{padding: 8, color: 'black'}}>
-                  <Entypo name="attachment" size={15} color="black" />
-                  {'  '}
-                    No Attachments
-                </Text>:
+          {content.obj.length == 0 ? (
+            <Text style={{padding: 8, color: 'black'}}>
+              <Entypo name="attachment" size={15} color="black" />
+              {'  '}
+              No Attachments
+            </Text>
+          ) : (
             content.obj.map((item, index) => {
               return (
-                <TouchableOpacity key={index+1} onPress={()=>{downloadFile(item.filePath,item.id)}}>
-
-                <Text style={{padding: 8, color: 'black'}}>
-                  <Entypo name="attachment" size={15} color="black" />
-                  {'  '}
-                  {item.fileName}
-                </Text>
+                <TouchableOpacity
+                  key={index + 1}
+                  onPress={() => {
+                    downloadFile(item.filePath, item.id);
+                  }}>
+                  <Text style={{padding: 8, color: 'black'}}>
+                    <Entypo name="attachment" size={15} color="black" />
+                    {'  '}
+                    {item.fileName}
+                  </Text>
                 </TouchableOpacity>
               );
-            }
+            })
           )}
         </View>
       </View>
-    {loginMember=='student' && type=='ASSIGNMENT'?<>
-      <TouchableOpacity onPress={docPicker} style={{width:170,height:50,justifyContent:'center',backgroundColor:'#0099cc',alignSelf:'center',marginTop:30,borderRadius:10}}>
+      {loginMember == 'student' && type == 'ASSIGNMENT' ? (
+        isFileSubmitted ? (
+          <Text
+            style={{
+              fontSize: 18,
+              height: 55,
+              paddingTop: 4,
+              fontWeight: '700',
+              color: 'white',
+              textAlign: 'center',
+              marginTop: 50,
+              backgroundColor: '#0099cc',
+              width: 200,
+              alignSelf: 'center',
+              borderRadius: 10,
+            }}>
+            Assignment Already Submitted
+          </Text>
+        ) : (
+          <>
+            <TouchableOpacity
+              onPress={docPicker}
+              style={{
+                width: 170,
+                height: 50,
+                justifyContent: 'center',
+                backgroundColor: '#0099cc',
+                alignSelf: 'center',
+                marginTop: 30,
+                borderRadius: 10,
+              }}>
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 15,
+                  textAlign: 'center',
+                  fontWeight: '700',
+                }}>
+                Attach Files ({attachmentCount})
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              disabled={loading}
+              onPress={documentUpload}
+              style={{
+                width: 170,
+                height: 50,
+                justifyContent: 'center',
+                backgroundColor: '#0050ee',
+                alignSelf: 'center',
+                marginTop: 30,
+                borderRadius: 10,
+              }}>
+              {loading ? (
+                <ActivityIndicator size={30} color="white" />
+              ) : (
+                <Text
+                  style={{
+                    color: 'white',
+                    fontSize: 15,
+                    textAlign: 'center',
+                    fontWeight: '700',
+                  }}>
+                  Submit Assignment
+                </Text>
+              )}
+            </TouchableOpacity>
+          </>
+        )
+      ) : (
+        <View style={{width: '100%', height: '100%'}}>
+          <Text
+            style={{padding: 10, fontSize: 20, color: 'black', marginTop: 20}}>
+            Assignments Submitted
+          </Text>
 
-        <Text style={{color:'white',fontSize:15,textAlign:'center',fontWeight:'700'}}>Attach Files ({attachmentCount})</Text>
-      </TouchableOpacity>
-      <TouchableOpacity         disabled={loading} onPress={documentUpload} style={{width:170,height:50,justifyContent:'center',backgroundColor:'#0050ee',alignSelf:'center',marginTop:30,borderRadius:10}}>
-
-      {loading?<ActivityIndicator size={30} color='white'/> :<Text style={{color:'white',fontSize:15,textAlign:'center',fontWeight:'700'}}>Submit Assignment</Text>
-      }
-      </TouchableOpacity>
-    </>:null}  
+          <ScrollView
+            style={{width: '100%', backgroundColor: '#ffffff'}}
+            showsVerticalScrollIndicator={false}>
+            {userAssignment.map((item, index) => {
+              return (
+                <TouchableOpacity
+                  style={{
+                    marginTop: 2,
+                    marginBottom: 2,
+                    elevation: 1.5,
+                    flexDirection: 'row',
+                    padding: 10,
+                    width: '95%',
+                    alignSelf: 'center',
+                  }}>
+                  <View style={{width: '20%'}}>
+                    <Text
+                      key={index + 1}
+                      style={{color: 'black', fontSize: 15, fontWeight: '600'}}>
+                      {item.studentId + ' |'}
+                    </Text>
+                  </View>
+                  <View style={{width: '80%'}}>
+                    <Text
+                      key={index + 1}
+                      style={{color: 'black', fontSize: 15}}>
+                      {item.fileName}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
+        </View>
+      )}
     </View>
   );
 };
